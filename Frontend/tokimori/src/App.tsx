0fb94b8d7746b691af980beb9888065994b486dc
Jose Carlos Ruiz Sanchez <jrs407@inlumine.ual.es>
2026-03-02 16:49:06 +0100

    Empezada modificación frontend

--- Frontend/tokimori/src/App.tsx
+++ Frontend/tokimori/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { Login } from './pages/Login'
 import { Register } from './pages/Register'
+import { Home } from './pages/Home'
 import './App.css'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
---
+++ Frontend/tokimori/src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../hooks/useAuth';
+import { validateLoginForm } from '../../utils/validation';
+import styles from './Home.module.css';
+
+export const Home = () => {
+      const navigate = useNavigate();
+
+      return (
+        <div></div>
+      );
+};
